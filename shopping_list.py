5b4ac42042679e3064f68f8943d02d9faa6f3cfd
MaciejHoroszkiewicz <horoszkiewicz.maciej@gmail.com>
2023-02-17 02:38:59 +0100

    Create for loop

--- shopping_list.py
+++ shopping_list.py
@@ -3,3 +3,13 @@ shopping_dict = {"piekarnia": ["chleb", "pączek", "bułki"],
                  "warzywniak": ["marchew", "seler", "rukola"],
                  }
 print(shopping_dict)
+
+print("")
+
+num_items = []
+
+for store, items in shopping_dict.items():
+    item_list = [item.title() for item in items]
+    for i in items:
+        num_items.append(i)
+    print(f"Idę do {store.title()} i kupuję {item_list}.")
